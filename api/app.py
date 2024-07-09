f2bb41910b2ceab61477f92affe9f70972f1f9bc
PsicoDevXD <168474127+PsicoDevXD@users.noreply.github.com>
2024-07-09 17:01:19 -0300

    Update app.py 11

--- api/app.py
+++ api/app.py
@@ -26,7 +26,7 @@ def load_data():
             {"id": 2, "nome": "Desenhada", "preco": "R$30"},
             {"id": 3, "nome": "Completa", "preco": "R$35"}
         ],
-        "outrosServicos": [
+        "outrosservicos": [
             {"id": 1, "nome": "Design de sobrancelhas", "preco": "R$20"},
             {"id": 2, "nome": "Limpeza de sobrancelhas", "preco": "R$25"}
         ]
